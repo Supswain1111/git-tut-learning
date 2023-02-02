c0db96febaab4827e236874196658952b2b129da
Supswain1111 <supravas952@gmail.com>
2023-02-02 20:27:35 +0530

    commit

--- demo.js
+++ demo.js
@@ -1,1 +1,7 @@
-console.log("hello");
+var Items=document.getElementsByClassName("list-group-item")
+console.log(Items)
+Items[0].style.fontWeight='bold';
+Items[1].style.fontWeight='bold';
+Items[2].style.fontWeight='bold';
+Items[3].style.fontWeight='bold';
+Items[2].style.background='green';
