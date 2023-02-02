46c6345583e84dbd1c29a4d477e74991a07b61d7
Supswain1111 <supravas952@gmail.com>
2023-02-02 20:39:19 +0530

    hi

--- demo.js
+++ demo.js
@@ -1,7 +1,10 @@
 var Items=document.getElementsByClassName("list-group-item")
 console.log(Items)
-Items[0].style.fontWeight='bold';
-Items[1].style.fontWeight='bold';
-Items[2].style.fontWeight='bold';
-Items[3].style.fontWeight='bold';
+
+for(var i=0;i<Items.length;i++)
+{
+    Items[i].style.fontWeight='bold';
+}
 Items[2].style.background='green';
+var li = document.getElementById('li')
+console.log(li)
