f8060347224a0e68644924877c404ea7b8e20142
Supswain1111 <supravas952@gmail.com>
2023-02-09 20:37:32 +0530

    storing object

--- file.html/work 3/main.js
+++ file.html/work 3/main.js
@@ -2,7 +2,11 @@ function saveToLocalStorage(event){
     event.preventDefault();
     const Name=event.target.name.value;
     const EmailID=event.target.email.value;
+    const my_obj = {
+        Name,
+        EmailID
+    }
 
-    localStorage.setItem('name',Name);
-    localStorage.setItem('email',EmailID);
+    localStorage.setItem('userDetails', JSON.stringify(my_obj));
 }
+    
